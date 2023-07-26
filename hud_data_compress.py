bf83ed99e554ab7367eadbd9b41ffe06610b186e
Shak Ragoler <shak.ragoler@gmail.com>
2023-07-25 20:54:04 -0700

    adding comments

--- hud_data_compress.py
+++ hud_data_compress.py
@@ -1,29 +1,40 @@
 import pandas as pd
 import requests
 import json
-import map_testing as mt
+import map_info as mi
 
 
+# Loads data
 def load_compressed(db):
+    # Read the CSV
     df = pd.read_csv(db)
+
+    # Drop the duplicates
     df["address1"] = df["address1"].astype(str).str.title()
     df["city"] = df["city"].astype(str).str.title()
     df = df.drop_duplicates(subset=["Organization Name", "address1", "city", "State", "ServiceSummary"], keep="first")
+
+    # Merge services with the same summary, and fill in blanks
     df = df.groupby(["Organization Name", "address1", "city", "State", "zip"]).agg({"ServiceSummary": ", ".join}).reset_index()
     df = df.replace("Nan", "", regex=True)
     df.columns = df.columns.str.lower()
     return df
 
 
+# Determine if a resource in the CSV is already on Shelter App
 def check_database(name, address1, city, state):
+    # Remove parentheses from the name
     if "(" in name:
         name = name[0:name.find("(")]
 
     if ")" in name:
         name = name[0:name.find(")")]
 
+    # Search for a resource on Shelter App
     request_list = requests.get("https://api.shelter.app/services?filter=isApproved&isApproved=true&limit=100&skip=0&search=name,address1,address2,city,state,zip,serviceSummary,category,age&q=" + name)
     data_dict = json.loads(request_list.content)
+
+    # Return true if there is a resource in Shelter App that matches a resource from the CSV
     for data in data_dict:
         if address1 == data["address1"] and city == data["city"] and state == data["state"]:
             return True
@@ -31,24 +42,37 @@ def check_database(name, address1, city, state):
     return False
 
 
-df = load_compressed("hud-data.csv")
+def main():
+    # Load the data
+    df = load_compressed("hud-data.csv")
 
-deletion = []
-deleted = 0
-undeleted = 0
-for i in range(len(df)):
-    r = df.iloc[i]
-    if check_database(r["organization name"], r["address1"], r["city"], r["state"]):
-        deletion.append(i)
-        deleted += 1
-        if i % 1000 == 0:
+    deletion = []
+    deleted = 0
+    undeleted = 0
+
+    for i in range(len(df)):
+        r = df.iloc[i]
+
+        # Add any resource from the CSV that appears on Shelter App to the list of resources to be deleted
+        if check_database(r["organization name"], r["address1"], r["city"], r["state"]):
+            deletion.append(i)
+            deleted += 1
             print("Found a copy of " + r["organization name"] + " in the database " + str(deleted))
-    else:
-        undeleted += 1
-        if i % 1000 == 0:
+        else:
+            undeleted += 1
             print("Did not find a copy of " + r["organization name"] + " in the database " + str(undeleted))
 
-print(str(undeleted) + " will remain and " + str(deleted) + " will be deleted")
-df = df.drop(deletion)
-df = mt.get_expanded_df(df)
-df.to_csv("hud-data-compressed.csv", sep=",")
+    print(str(undeleted) + " will remain and " + str(deleted) + " will be deleted")
+
+    # Delete resources that already appear on Shelter App
+    df = df.drop(deletion)
+
+    # Add info on the URLs and schedules of resources
+    df = mi.get_expanded_df(df)
+
+    # Create the final CSV to be used on Shelter App
+    df.to_csv("hud-data-compressed.csv", sep=",")
+
+
+if __name__ == "__main__":
+    main()
--- map_info.py
+++ map_info.py
@@ -5,43 +5,62 @@ from datetime import datetime
 map_client = googlemaps.Client(key="<YOUR KEY>")
 
 
+# Get info on a location on Google Maps
 def get_place_info(location_name):
     try:
+        # Use the Google Maps API to get raw data on a location
         response = map_client.places(query=location_name)
         results = response.get("results")
+
+        # Stop if there is no info on a location on Google Maps
         if len(results) == 0:
             return None
+
+        # Retrieve specific details on a location from Google Maps
         place_id = results[0]["place_id"]
         place_details = map_client.place(place_id=place_id)
         return place_details["result"]
+
+    # Throw an exception something goes wrong with the request
     except Exception as e:
         print(e)
         return None
 
 
+# Get the schedule of a location on Google Maps
 def get_schedule(schedule_list):
     if schedule_list is not None and "current_opening_hours" in schedule_list:
         schedule = ""
+
+        # Get hourly information for each day of the week
         for day in schedule_list["current_opening_hours"]["weekday_text"]:
             if isinstance(day, str):
+                # Format the text in a way that is readable on the Shelter App dataset
                 schedule = schedule + day.replace('\u2009', '').replace('\u202f', '') + " "
+
         return schedule
     return ""
 
 
+# Add URLs and schedules to the dataframe used to make the final CSV
 def get_expanded_df(df):
     scheds = []
     urls = []
+
     for i in range(len(df)):
+        # Get specific details on a location on Google Maps and add the schedule to the list of schedules
         place_details = get_place_info(df.iloc[i]["organization name"] + " " + df.iloc[i]["address1"] + " " + df.iloc[i]["city"] + " " + df.iloc[i]["state"] + " " + str(df.iloc[i]["zip"]))
         print(i)
         scheds.append(get_schedule(place_details))
+
+        # Get the URL of a location on Google Maps
         if place_details is not None and "website" in place_details:
             urls.append(place_details["website"])
         else:
             urls.append("")
-        if i % 1000 == 0:
-            print(place_details)
+
+        print(place_details)
+
     print("There are " + str(len(df)) + " entries")
     print("There are " + str(len(scheds)) + " schedules")
     print("There are " + str(len(urls)) + " URLs")
